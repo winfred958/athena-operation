3f43ff19ee7a2f5d393c1d0ae0d9065262e0698e
kevin <kevin_678@126.com>
2020-03-03 09:11:30 +0800

    fix: bug

--- athena-operation/athena_opertation.py
+++ athena-operation/athena_opertation.py
@@ -22,12 +22,12 @@ def get_parse_args():
     parser.add_argument("-t", "--table", help="table", action="store",
                         type=str, required=True)
 
-    parser.add_argument("-pf", "--partition-format", help="eg. year='%Y',month='%m',day='%d' OR dt='%Y-%m-%d'",
+    parser.add_argument("-pf", "--partition-format", help="eg. year='%%Y',month='%%m',day='%%d' OR dt='%%Y-%%m-%%d'",
                         action="store",
                         type=str, default=None, required=True)
 
     parser.add_argument("-lf", "--location-format",
-                        help="location: s3://xxxx/database/table/dt=%Y-%m-%d OR s3://xxxx/database/table/%Y/%m/%d",
+                        help="location: s3://xxxx/database/table/dt=%%Y-%%m-%%d OR s3://xxxx/database/table/%%Y/%%m/%%d",
                         action="store",
                         type=str, default=None, required=False)
 
