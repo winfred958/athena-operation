9798313edc5091c1ee7b3cb01142b75a4495d480
kevin <kevin_678@126.com>
2020-03-03 12:01:29 +0800

    fix: bug

--- bin/add_partition.sh
+++ bin/add_partition.sh
@@ -4,4 +4,5 @@ PROJECT_ROOT_PATH=$(cd "$(dirname "$0")"; cd ..; cd athena-operation/;pwd)
 
 source "${PROJECT_ROOT_PATH}/config/env.sh"
 
+echo -e "${PROJECT_ROOT_PATH}/athena-operation/athena_opertation.py \\\n${*}"
 python3 "${PROJECT_ROOT_PATH}/athena-operation/athena_opertation.py" "${*}"
