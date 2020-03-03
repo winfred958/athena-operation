8c07620e617baff7a060f3557c2731a35044020d
kevin <kevin_678@126.com>
2020-03-03 12:32:16 +0800

    feat: udpate shell

--- bin/add_partition.sh
+++ bin/add_partition.sh
@@ -4,5 +4,6 @@ PROJECT_ROOT_PATH=$(cd "$(dirname "$0")"; cd ..; cd athena-operation/;pwd)
 
 source "${PROJECT_ROOT_PATH}/config/env.sh"
 
-echo -e "${PROJECT_ROOT_PATH}/athena-operation/athena_opertation.py \\\n${*}"
-python3 "${PROJECT_ROOT_PATH}/athena-operation/athena_opertation.py" "${*}"
+CMD="python3 ${PROJECT_ROOT_PATH}/athena-operation/athena_opertation.py ${*}"
+echo -e "${CMD}"
+${CMD}
